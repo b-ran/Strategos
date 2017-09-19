f677806ff91eaa3724d1fd392d8c91a7b23fceb4
Devon Mortimer <devon.mortimer@gmail.com>
2017-09-19 21:53:46 +1200

    Add basic behaviour handler

---
+++ src/main/java/strategos/behaviour/BehaviourBase.java
@@ -0,0 +1,19 @@
+package strategos.behaviour;
+
+
+import strategos.model.*;
+
+
+class BehaviourBase implements Behaviour {
+
+    private Hex position;
+
+    @Override public Hex getPosition() {
+        assert position != null : "Position should never be null on retrieval.";
+        return position;
+    }
+
+    @Override public void setPosition(Hex position) {
+        this.position = position;
+    }
+}
