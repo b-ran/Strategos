b907005e3b9b6e7a5295dc240a1de35fa97ce363
Daniel Pinfold <daniel@gilbertpinfold.co.nz>
2017-09-21 12:09:42 +1200

    Added javadocs.

--- src/main/java/strategos/hexgrid/Map.java
+++ src/main/java/strategos/hexgrid/Map.java
@@ -10,19 +10,35 @@ import java.util.List;
 
 import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Collections;
 
+/**
+ * A wrapper for a 2D array of Hexes. The corners of this array are NullHexes, simulating a hexagonal grid.
+ * 		Since a grid of tessellated hexagons tapers towards the top and bottom, this offset increases towards the 
+ * 		top-left and bottom-right corners. This Map uses flat-topped hexagons, and calculates neighbours based on
+ * 		axial coordinates.
+ * 
+ * @author Daniel Pinfold
+ *
+ */
 public class Map {
 	
 	private Hex[][] map;
 	private final int radius;
 	
+	/**
+	 * Creates a new Map object with the specified diameter.
+	 * @param diameter - The maximum size of a row of Hexes.
+	 */
 	public Map(int diameter) {
 		map = constructMap(diameter);
 		radius = diameter / 2;
 	}
 	
 	/**
-	 * Creates a 2D array of Hex objects
-	 * @return
+	 * Creates a 2D array of Hex objects. Has an offset of radius-1, where the top left
+	 * 		and bottom right corners are NullHexes, up to the size of the offset. This simulates
+	 * 		a grid of tessellated hexagons.
+	 * 
+	 * @return A 2D array of Hexes.
 	 */
 	private Hex[][] constructMap(int diameter) {
 		
@@ -78,6 +94,14 @@ public class Map {
 		return map;
 	}
 	
+	/**
+	 * For a given Hex at (r, q), calculate all the neighbours using the axial coordinates system.
+	 * 		A map is passed into this function because the map field may not be initialised at this point.
+	 * 
+	 * @param r - The horizontal position of this Hex.
+	 * @param q - The vertical position of this Hex.
+	 * @param map - The map value.
+	 */
 	private void populateNeighbours(int r, int q, Hex[][] map) {
 		get(r, q, map).addNeighbour(EAST, get(r + 1, q, map));
 		get(r, q, map).addNeighbour(WEST, get(r - 1, q, map));
@@ -108,6 +132,10 @@ public class Map {
 		return map;
 	}
 	
+	/**
+	 * Combines the 2D array into a List format, reading left to right, then dropping a line.
+	 * @return A List of Hexes contained by the Map.
+	 */
 	public List<Hex> getMapAsList() {
 		List<Hex> temp = new ArrayList<>();
 		for (int x = 0; x < map.length; x++) {
