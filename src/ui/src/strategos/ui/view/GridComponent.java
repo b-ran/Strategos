d48b91eb4cd042ff55def544fea15107cd285926
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2017-09-23 20:19:42 +1200

    Add start of drawing hexgrid

--- src/ui/src/strategos/ui/view/GridComponent.java
+++ src/ui/src/strategos/ui/view/GridComponent.java
@@ -1,5 +1,7 @@
 package strategos.ui.view;
 
+import strategos.terrain.Terrain;
+
 import javax.swing.*;
 
 import java.awt.*;
@@ -29,5 +31,48 @@ public class GridComponent extends JComponent {
         p.setLayout(new BorderLayout());
         p.setPreferredSize(GRID_COMPONENT_SIZE);
         return p;
+    }
+
+    protected void paintComponent(Graphics g) {
+        paintHexGrid(g, Color.BLACK, null);
+        System.out.println("Paint");
+    }
+
+    private void paintHexGrid(Graphics g, Color c, Terrain[][] terrain) {
+        /*g.setColor(Color.BLACK);
+        for (int y = 0; y < terrain.length; y++) {
+            for (int x = 0; x < terrain[0].length; x++) {
+                hexagon(g, x, y, Color.BLACK);
+            }
+        }*/
+
+        g.setColor(Color.BLACK);
+        for (int y = 0; y < 10; y++) {
+            for (int x = 0; x < 10; x++) {
+                if (y % 2 == 0) {
+                    hexagon(g, getGridX(x), getGridY(y), Color.BLACK);
+                } else {
+                    hexagon(g, getGridX(x)+HEX_SIZE/2, getGridY(y), Color.BLACK);
+                }
+
+            }
+        }
+    }
+
+    private void hexagon(Graphics g, int x, int y, Color c) {
+        int nPoints = 6;
+        int[] xPoints = {x, x+HEX_SIZE/2, x+HEX_SIZE, x+HEX_SIZE, x+HEX_SIZE/2, x, x};
+        int[] yPoints = {y+HEX_SIZE/4, y, y+HEX_SIZE/4, y+HEX_SIZE/4*3, y+HEX_SIZE, y+HEX_SIZE/4*3, y+HEX_SIZE/4};
+        g.setColor(c);
+        g.fillPolygon(xPoints, yPoints, nPoints);
+    }
+
+    //Credit: https://www.redblobgames.com/grids/hexagons/#hex-to-pixel for logic of hex to pixels
+    private int getGridY(int y) {
+        return HEX_SIZE/2 * 3/2 * y;
+    }
+
+    private int getGridX(int x) {
+        return x * HEX_SIZE + HEX_SIZE;
     }
 }
