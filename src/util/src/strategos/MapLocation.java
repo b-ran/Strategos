4a0e189b4129d44ceb2fcf4e318bbd97980f2e74
Daniel Pinfold <daniel@gilbertpinfold.co.nz>
2017-09-23 17:52:35 +1200

    Add getNeighbour method for MapLocation API

--- src/util/src/strategos/MapLocation.java
+++ src/util/src/strategos/MapLocation.java
@@ -5,4 +5,13 @@ public interface MapLocation {
     int getX();
 
     int getY();
+
+    /**
+     * Get the neighbour at the specified orientation relative to this MapLocation.
+     * Return a NullHex if no neighbour exists at that position.
+     *
+     * @param direction - The Direction that the desired MapLocation is, relative to this MapLocation.
+     * @return A MapLocation at the specified Direction.
+     */
+    MapLocation getNeighbour(Direction direction);
 }
