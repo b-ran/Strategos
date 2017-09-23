2d79ebc51ef6e3eb6d74188c6a682b5e4dcbd3f6
Shaun Sinclair <sdmsinclair@gmail.com>
2017-09-23 18:16:14 +1200

    merged

--- src/mapcreation/src/mapcreation/noisegeneration/NoiseGenerator.java
+++ src/mapcreation/src/mapcreation/noisegeneration/NoiseGenerator.java
@@ -6,7 +6,7 @@ package mapcreation.noisegeneration;
  * 28/08/2017.
  */
 public class NoiseGenerator {
-    //TODO find out how to do for hex map insted of square
+    //TODO find out how to do for hex map instead of square
     public double[][] generateNoise(int height, int width){
         return new double[8][8];
     }
