a8ee7ae4fd3786c0be98d876f0455e86599198ca
Devon Mortimer <devon.mortimer@gmail.com>
2017-09-23 17:32:34 +1200

    Change signature of move methods to accept direction parameter

--- src/util/src/strategos/behaviour/Behaviour.java
+++ src/util/src/strategos/behaviour/Behaviour.java
@@ -1,7 +1,7 @@
 package strategos.behaviour;
 
 
-import strategos.MapLocation;
+import strategos.*;
 import strategos.units.*;
 
 
@@ -19,7 +19,7 @@ public interface Behaviour {
 
     void charge();
 
-    boolean move();
+    boolean move(Direction direction);
 
     int attack(Unit enemy);
 
