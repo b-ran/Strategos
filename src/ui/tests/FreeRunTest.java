4d0aee5856023729464beec1d26321818ba47fe2
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2017-09-24 16:28:04 +1300

    Updated FreeRunTest to work with Owner and Maplocation Objects

--- src/ui/tests/FreeRunTest.java
+++ src/ui/tests/FreeRunTest.java
@@ -1,6 +1,7 @@
+import strategos.UnitOwner;
 import strategos.terrain.Terrain;
 import strategos.ui.Ui;
-import strategos.units.Unit;
+import strategos.units.*;
 import terrain.*;
 import units.*;
 
@@ -18,10 +19,28 @@ public class FreeRunTest {
                 {new RiverTestObj(),    new RiverTestObj(),     new RiverTestObj(),     new RiverTestObj()}
         };
 
-        entities.add(new ArchersTestObj());
-        entities.add(new CavalryTestObj());
-        entities.add(new EliteTestObj());
-        entities.add(new SpearmenTestObj());
+        UnitOwnerTestObj owner = new UnitOwnerTestObj();
+
+        ArchersTestObj a = new ArchersTestObj(owner);
+        CavalryTestObj c = new CavalryTestObj(owner);
+        EliteTestObj e = new EliteTestObj(owner);
+        SpearmenTestObj s = new SpearmenTestObj(owner);
+
+        owner.addUnit(a);
+        owner.addUnit(c);
+        owner.addUnit(e);
+        owner.addUnit(s);
+
+        a.setPosition(new MapLocationTestObj(0,0));
+        c.setPosition(new MapLocationTestObj(1,1));
+        e.setPosition(new MapLocationTestObj(2,2));
+        s.setPosition(new MapLocationTestObj(3,3));
+
+
+        entities.add(a);
+        entities.add(c);
+        entities.add(e);
+        entities.add(s);
 
         Ui ui = new Ui(entities,terrain);
     }
