1fe0d6ade1f22cda2ec86372d33d5ef28b773c8e
Daniel Pinfold <daniel@gilbertpinfold.co.nz>
2017-09-26 10:17:04 +1300

    Create test for getting tiles in range > 1

--- src/model/src/strategos/model/tests/Tests.java
+++ src/model/src/strategos/model/tests/Tests.java
@@ -1,6 +1,7 @@
 package strategos.model.tests;
 
 import org.junit.Test;
+import strategos.Direction;
 import strategos.GameCollections;
 import strategos.MapLocation;
 import strategos.hexgrid.Map;
@@ -43,7 +44,7 @@ public class Tests {
 
 		MapLocation mid = world.getMap().get(3, 3);
 
-		List<MapLocation> hexes = gameState.getTilesInRange(mid, 1);
+		List<MapLocation> hexes = gameState.getTilesInRange(mid, 2);
 
 		assertTrue(hexes.contains(mid));
 		assertTrue(hexes.contains(mid.getNeighbour(NORTH_EAST)));
@@ -53,12 +54,15 @@ public class Tests {
 		assertTrue(hexes.contains(mid.getNeighbour(SOUTH_EAST)));
 		assertTrue(hexes.contains(mid.getNeighbour(SOUTH_WEST)));
 
-		assertTrue(hexes.contains(mid.getNeighbour(NORTH_EAST)));
-		assertTrue(hexes.contains(mid.getNeighbour(NORTH_WEST)));
-		assertTrue(hexes.contains(mid.getNeighbour(EAST)));
-		assertTrue(hexes.contains(mid.getNeighbour(WEST)));
-		assertTrue(hexes.contains(mid.getNeighbour(SOUTH_EAST)));
-		assertTrue(hexes.contains(mid.getNeighbour(SOUTH_WEST)));
+		for (java.util.Map.Entry<Direction, MapLocation> entry : mid.getNeighbours().entrySet()) {
+			assertTrue(hexes.contains(entry.getValue()));
+			assertTrue(hexes.contains(entry.getValue().getNeighbour(NORTH_EAST)));
+			assertTrue(hexes.contains(entry.getValue().getNeighbour(NORTH_WEST)));
+			assertTrue(hexes.contains(entry.getValue().getNeighbour(EAST)));
+			assertTrue(hexes.contains(entry.getValue().getNeighbour(WEST)));
+			assertTrue(hexes.contains(entry.getValue().getNeighbour(SOUTH_EAST)));
+			assertTrue(hexes.contains(entry.getValue().getNeighbour(SOUTH_WEST)));
+		}
 	}
 
 }
