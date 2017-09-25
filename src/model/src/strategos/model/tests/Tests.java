61b868d76567c12d113ef2e059ada9b17389be87
Daniel Pinfold <daniel@gilbertpinfold.co.nz>
2017-09-25 17:43:34 +1300

    Add tests for model

---
+++ src/model/src/strategos/model/tests/Tests.java
@@ -0,0 +1,18 @@
+package strategos.model.tests;
+
+import org.junit.Test;
+import strategos.Direction;
+import strategos.MapLocation;
+import strategos.hexgrid.Map;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class Tests {
+
+	@Test
+	public void inRangeTest_1() {
+
+	}
+
+}
