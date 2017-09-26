7cbdae744de101d0dabf18fb254a0a79ee72dc60
Shaun Sinclair <sdmsinclair@gmail.com>
2017-09-26 22:10:17 +1300

    wrote river adding code and made a secondary constructor that doesn't take a seed

--- src/mapcreation/test/TestAll.java
+++ src/mapcreation/test/TestAll.java
@@ -1,2 +1,12 @@
+import mapgenerationtests.MapTester;
+import noisegenerationtests.NoiseTester;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        NoiseTester.class,
+        MapTester.class})
 public class TestAll {
+
 }
--- src/mapcreation/test/noisegenerationtests/NoiseTester.java
+++ src/mapcreation/test/noisegenerationtests/NoiseTester.java
@@ -68,4 +68,16 @@ public class NoiseTester {
         }
     }
 
+    @Test
+    public void genericTest() {
+        int width = 7, height = 7;
+        int x = 0, y = 0;
+        for (int i = 0; i < width + height - 2; i++) {
+            if (i % 2 == 0) x++;
+            else y++;
+            System.out.println("[" + x + "][" + y + "]");
+        }
+
+    }
+
 }
