ee35c7a97325e0024966fb5deae29f0a0fe339ec
Daniel Pinfold <daniel@gilbertpinfold.co.nz>
2017-09-27 09:08:01 +1300

    Create Bridge API interface and implementation

---
+++ src/model/src/strategos/model/units/BridgeImpl.java
@@ -0,0 +1,10 @@
+package strategos.model.units;
+
+import strategos.UnitOwner;
+import strategos.units.Bridge;
+
+public class BridgeImpl extends UnitImpl implements Bridge {
+	public BridgeImpl(UnitOwner owner) {
+		super(owner);
+	}
+}
