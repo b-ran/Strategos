2af07ca0d0d0c26aca60b78491ef286b56562ce2
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2017-09-30 21:39:05 +1300

    Add test objects to fit with terrian being a maplocation

---
+++ src/ui/tests/model/GameCollectionTestObj.java
@@ -0,0 +1,40 @@
+package model;
+
+import strategos.GameBoard;
+import strategos.GameCollections;
+import strategos.units.Unit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameCollectionTestObj implements GameCollections {
+
+    GameBoard gameBoard;
+    List<Unit> allUnits = new ArrayList<>();
+
+    @Override
+    public GameBoard getMap() {
+        return gameBoard;
+    }
+
+    @Override
+    public List<Unit> getAllUnits() {
+        return allUnits;
+    }
+
+    @Override
+    public void setMap(GameBoard map) {
+        gameBoard = map;
+    }
+
+    @Override
+    public void setAllUnits(ArrayList<Unit> allUnits) {
+        this.allUnits = allUnits;
+    }
+
+    public void setAllUnits(List<Unit> allUnits) {
+        this.allUnits = allUnits;
+    }
+
+}
+
