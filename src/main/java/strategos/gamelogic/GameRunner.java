c8d6092dd36357ec02e2caec405fa3a111a8dbda
Daniel Pinfold <daniel@gilbertpinfold.co.nz>
2017-10-04 18:08:28 +1300

    Wire together libraries (bar networking) into a semicomplete game

--- src/main/java/strategos/gamelogic/GameRunner.java
+++ src/main/java/strategos/gamelogic/GameRunner.java
@@ -5,6 +5,8 @@ import mapcreation.mapgeneration.TerrainGeneration;
 import strategos.GameState;
 import strategos.Paintable;
 import strategos.UnitOwner;
+import strategos.behaviour.BehaviourFactory;
+import strategos.behaviour.BehaviourFactoryImpl;
 import strategos.hexgrid.Map;
 import strategos.model.Player;
 import strategos.model.Strategos;
@@ -22,6 +24,7 @@ public class GameRunner {
 
 	private Ui ui;
 	private GameState gameState;
+	private BehaviourFactory factory;
 
 	public GameRunner() {
 		TerrainGeneration generator = new TerrainGeneration();
@@ -41,10 +44,13 @@ public class GameRunner {
 		}
 		World world = new World(map, allUnits);
 		gameState = new Strategos(world, players.get(0), players.get(1), players.get(2));
+
+		factory = new BehaviourFactoryImpl();
+
 		ui = new Ui(gameState);
 	}
 
-	public static List<UnitOwner> constructPlayers() {
+	public List<UnitOwner> constructPlayers() {
 		List<UnitOwner> unitOwners = new ArrayList<>();
 		unitOwners.add(new Player(false));
 		unitOwners.add(new Player(false));
@@ -59,26 +65,31 @@ public class GameRunner {
 		return unitOwners;
 	}
 
-	private static List<Unit> constructUnits(UnitOwner player) {
+	private List<Unit> constructUnits(UnitOwner player) {
 		List<Unit> units = new ArrayList<>();
-		units.add(new SwordsmenImpl(null, player));
-		units.add(new SwordsmenImpl(null, player));
-		units.add(new SpearmenImpl(null, player));
-		units.add(new SpearmenImpl(null, player));
-		units.add(new CavalryImpl(null, player));
-		units.add(new EliteImpl(null, player));
-		units.add(new ArchersImpl(null, player));
-		units.add(new ArchersImpl(null, player));
+		units.add(new SwordsmenImpl(factory.createBehaviourSwordsmen(gameState), player));
+		units.add(new SwordsmenImpl(factory.createBehaviourSwordsmen(gameState), player));
+
+		units.add(new SpearmenImpl(factory.createBehaviourSpearmen(gameState), player));
+		units.add(new SpearmenImpl(factory.createBehaviourSpearmen(gameState), player));
+
+		units.add(new CavalryImpl(factory.createBehaviourCavalry(gameState), player));
+
+		units.add(new EliteImpl(factory.createBehaviourElite(gameState), player));
+
+		units.add(new ArchersImpl(factory.createBehaviourArchers(gameState), player));
+		units.add(new ArchersImpl(factory.createBehaviourArchers(gameState), player));
 
 		return units;
 	}
 
-	private static List<Unit> constructBarbarianUnits(UnitOwner player) {
+	private List<Unit> constructBarbarianUnits(UnitOwner player) {
 		List<Unit> units = new ArrayList<>();
-		units.add(new SwordsmenImpl(null, player));
-		units.add(new SwordsmenImpl(null, player));
-		units.add(new SpearmenImpl(null, player));
-		units.add(new SpearmenImpl(null, player));
+		units.add(new SwordsmenImpl(factory.createAiBehaviour(gameState, factory::createBehaviourSwordsmen), player));
+		units.add(new SwordsmenImpl(factory.createAiBehaviour(gameState, factory::createBehaviourSwordsmen), player));
+
+		units.add(new SpearmenImpl(factory.createAiBehaviour(gameState, factory::createBehaviourSpearmen), player));
+		units.add(new SpearmenImpl(factory.createAiBehaviour(gameState, factory::createBehaviourSpearmen), player));
 
 		return units;
 	}
