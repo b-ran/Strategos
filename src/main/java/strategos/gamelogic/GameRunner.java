81f4534ba319a78f57a0870f734b86fe3fe78e1e
Daniel Pinfold <daniel@gilbertpinfold.co.nz>
2017-10-04 18:16:00 +1300

    Fix circular dependency of Behaviour in GameRunner

--- src/main/java/strategos/gamelogic/GameRunner.java
+++ src/main/java/strategos/gamelogic/GameRunner.java
@@ -3,8 +3,10 @@ package strategos.gamelogic;
 
 import mapcreation.mapgeneration.TerrainGeneration;
 import strategos.GameState;
+import strategos.MapLocation;
 import strategos.Paintable;
 import strategos.UnitOwner;
+import strategos.behaviour.Behaviour;
 import strategos.behaviour.BehaviourFactory;
 import strategos.behaviour.BehaviourFactoryImpl;
 import strategos.hexgrid.Map;
@@ -28,6 +30,7 @@ public class GameRunner {
 
 	public GameRunner() {
 		TerrainGeneration generator = new TerrainGeneration();
+		factory = new BehaviourFactoryImpl();
 		Map map = new Map(MAP_DIAMETER);
 		Paintable[][] terrainMap = generator.populateMap(map.getData());
 
@@ -38,14 +41,18 @@ public class GameRunner {
 		}
 
 		List<UnitOwner> players = constructPlayers();
+
+		World world = new World(map, new ArrayList<>());
+		gameState = new Strategos(world, players.get(0), players.get(1), players.get(2));
+
+		populateUnits(gameState.getPlayers());
+
 		List<Unit> allUnits = new ArrayList<>();
 		for (UnitOwner player : players) {
 			allUnits.addAll(player.getUnits());
 		}
-		World world = new World(map, allUnits);
-		gameState = new Strategos(world, players.get(0), players.get(1), players.get(2));
 
-		factory = new BehaviourFactoryImpl();
+		world.setAllUnits(allUnits);
 
 		ui = new Ui(gameState);
 	}
@@ -56,18 +63,28 @@ public class GameRunner {
 		unitOwners.add(new Player(false));
 		unitOwners.add(new Player(true));
 
+		return unitOwners;
+	}
+
+	public void populateUnits(List<UnitOwner> unitOwners) {
 		for (int i = 0; i < 2; i++) {
 			unitOwners.get(i).setUnits(constructUnits(unitOwners.get(i)));
 		}
 
 		unitOwners.get(2).setUnits(constructBarbarianUnits(unitOwners.get(2)));
-
-		return unitOwners;
 	}
 
 	private List<Unit> constructUnits(UnitOwner player) {
 		List<Unit> units = new ArrayList<>();
-		units.add(new SwordsmenImpl(factory.createBehaviourSwordsmen(gameState), player));
+		MapLocation startLocation = gameState.getWorld().getMap().get(MAP_DIAMETER / 2, 1);
+
+		if (gameState.getPlayers().indexOf(player) == 0) {
+			startLocation = gameState.getWorld().getMap().get(MAP_DIAMETER / 2, MAP_DIAMETER - 1);
+		}
+		Behaviour behaviour = factory.createBehaviourSwordsmen(gameState);
+		units.add(new SwordsmenImpl(behaviour, player, startLocation));
+
+		/*units.add(new SwordsmenImpl(factory.createBehaviourSwordsmen(gameState), player));
 		units.add(new SwordsmenImpl(factory.createBehaviourSwordsmen(gameState), player));
 
 		units.add(new SpearmenImpl(factory.createBehaviourSpearmen(gameState), player));
@@ -78,18 +95,18 @@ public class GameRunner {
 		units.add(new EliteImpl(factory.createBehaviourElite(gameState), player));
 
 		units.add(new ArchersImpl(factory.createBehaviourArchers(gameState), player));
-		units.add(new ArchersImpl(factory.createBehaviourArchers(gameState), player));
+		units.add(new ArchersImpl(factory.createBehaviourArchers(gameState), player));*/
 
 		return units;
 	}
 
 	private List<Unit> constructBarbarianUnits(UnitOwner player) {
 		List<Unit> units = new ArrayList<>();
-		units.add(new SwordsmenImpl(factory.createAiBehaviour(gameState, factory::createBehaviourSwordsmen), player));
+		/*units.add(new SwordsmenImpl(factory.createAiBehaviour(gameState, factory::createBehaviourSwordsmen), player, gameState.getWorld().getMap().get()));
 		units.add(new SwordsmenImpl(factory.createAiBehaviour(gameState, factory::createBehaviourSwordsmen), player));
 
 		units.add(new SpearmenImpl(factory.createAiBehaviour(gameState, factory::createBehaviourSpearmen), player));
-		units.add(new SpearmenImpl(factory.createAiBehaviour(gameState, factory::createBehaviourSpearmen), player));
+		units.add(new SpearmenImpl(factory.createAiBehaviour(gameState, factory::createBehaviourSpearmen), player));*/
 
 		return units;
 	}
