f6b39bed8d5e28b4097fbd4230b12045abcbc60a
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2017-10-05 20:56:08 +1300

    Add attack functionally on attackable hexs

--- src/ui/src/strategos/ui/controller/AttackListener.java
+++ src/ui/src/strategos/ui/controller/AttackListener.java
@@ -1,16 +1,24 @@
 package strategos.ui.controller;
 
 
+import strategos.MapLocation;
+import strategos.units.Unit;
+
+import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.List;
 
 
 class AttackListener extends Controller implements MouseListener, KeyListener {
 
+    private Controller controller;
+
     AttackListener(Controller controller) {
         super(controller);
+        this.controller = controller;
     }
 
     @Override
@@ -20,7 +28,19 @@ class AttackListener extends Controller implements MouseListener, KeyListener {
 
     @Override
     public void mousePressed(MouseEvent e) {
+        if (controller.getSelectedMapLocation() == null) return;
 
+        Point p = getHexPos(e.getX(),e.getY());
+        Unit oldSelectedUnit = model.getUnitAt(controller.getSelectedMapLocation());
+        Unit newSelectedUnit = model.getUnitAt(board.get(p.x,p.y));
+
+        if (oldSelectedUnit == null || newSelectedUnit == null) return;
+
+        List<Unit> units = model.getUnitsInAttackRange(oldSelectedUnit);
+
+        if (units.contains(newSelectedUnit)) {
+            model.attack(oldSelectedUnit, newSelectedUnit.getPosition());
+        }
     }
 
     @Override
