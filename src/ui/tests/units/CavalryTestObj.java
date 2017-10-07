f5535a352150c681aff36b181308e7607eee484d
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2017-10-08 01:54:06 +1300

    Updated unit test objects to work with vistor pattern drawing

--- src/ui/tests/units/CavalryTestObj.java
+++ src/ui/tests/units/CavalryTestObj.java
@@ -1,10 +1,12 @@
 package units;
 
+import strategos.Graphical;
+import strategos.GraphicalVisitor;
 import strategos.UnitOwner;
 import strategos.behaviour.Behaviour;
 import strategos.units.Cavalry;
 
-public class CavalryTestObj extends UnitTestObj implements Cavalry {
+public class CavalryTestObj extends UnitTestObj implements Cavalry, Graphical {
     public CavalryTestObj(UnitOwner owner) {
         super(owner);
     }
@@ -12,5 +14,10 @@ public class CavalryTestObj extends UnitTestObj implements Cavalry {
     @Override
     public void setBehaviour(Behaviour behaviour) {
 
+    }
+
+    @Override
+    public void draw(GraphicalVisitor graphicalVisitor) {
+        graphicalVisitor.visit(this);
     }
 }
