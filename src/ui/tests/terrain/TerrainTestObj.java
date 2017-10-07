2623df15e8c30db60ff0c6dafc3e39786249864a
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2017-10-08 01:54:28 +1300

    Updated terrain test object to work with vistor pattern drawing

--- src/ui/tests/terrain/TerrainTestObj.java
+++ src/ui/tests/terrain/TerrainTestObj.java
@@ -1,6 +1,12 @@
 package terrain;
 
+import strategos.Graphical;
+import strategos.GraphicalVisitor;
 import strategos.terrain.Terrain;
 
-public class TerrainTestObj implements Terrain {
+public class TerrainTestObj implements Terrain, Graphical {
+    @Override
+    public void draw(GraphicalVisitor graphicalVisitor) {
+
+    }
 }
