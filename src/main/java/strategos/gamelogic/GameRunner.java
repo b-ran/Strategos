1638d0869cf596f8b1000146b54605469d78b02d
Daniel Pinfold <daniel@gilbertpinfold.co.nz>
2017-10-10 18:02:55 +1300

    Change GameRunner to full play mode instead of Cavalry debug

--- src/main/java/strategos/gamelogic/GameRunner.java
+++ src/main/java/strategos/gamelogic/GameRunner.java
@@ -108,10 +108,15 @@ public class GameRunner {
 		UnitOwner playerTwo = new Player(false);
 		UnitOwner barbarians = new Player(true);
 		GameState newState = new Strategos(new World(map, new ArrayList<>()), playerOne, playerTwo, barbarians);
-		Behaviour behaviour = factory.createBehaviourCavalry(newState);
+		/*Behaviour behaviour = factory.createBehaviourCavalry(newState);
 		playerOne.getUnits().add(new CavalryImpl(behaviour, playerOne, newState.getWorld().getMap().get(3, 6)));
-		behaviour = factory.createBehaviourSwordsmen(newState);
-		playerTwo.getUnits().add(new SwordsmenImpl(behaviour, playerTwo, newState.getWorld().getMap().get(5, 9)));
+		behaviour = factory.createBehaviourCavalry(newState);
+		playerTwo.getUnits().add(new CavalryImpl(behaviour, playerTwo, newState.getWorld().getMap().get(5, 8)));
+
+
+
+		newState.getWorld().getAllUnits().addAll(playerOne.getUnits());
+		newState.getWorld().getAllUnits().addAll(playerTwo.getUnits());*/
 
 		Bridge bridge = new BridgeImpl(factory.createBehaviourBridge(newState), barbarians, newState.getWorld().getMap().get(5,4));
 		newState.getWorld().getAllUnits().add(bridge);
@@ -123,9 +128,9 @@ public class GameRunner {
 		newState.getWorld().getAllUnits().add(bridge);
 		barbarians.getUnits().add(bridge);
 
-		newState.getWorld().getAllUnits().addAll(playerOne.getUnits());
-		newState.getWorld().getAllUnits().addAll(playerTwo.getUnits());
-		/*for (UnitOwner player : newState.getPlayers()) {
+		newState.getWorld().getAllUnits().addAll(barbarians.getUnits());
+
+		for (UnitOwner player : newState.getPlayers()) {
 			if (player.isNPC()) {
 				continue;
 			}
@@ -161,7 +166,7 @@ public class GameRunner {
 
 			player.setUnits(units);
 			newState.getWorld().getAllUnits().addAll(units);
-		}*/
+		}
 
 		return newState;
 	}
