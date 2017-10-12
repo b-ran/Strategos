df80cd1f6964cde8bf218a62003c53fd20b4be0c
Daniel Pinfold <daniel@gilbertpinfold.co.nz>
2017-10-12 19:01:44 +1300

    Adjust unit stats in config: swordsmen: STR 25->22 spearmen: TGH 25->30 archers: STR 18->20 cavalry: STR 30->20 elite: TGH 35->40

--- src/common/src/strategos/Config.java
+++ src/common/src/strategos/Config.java
@@ -15,20 +15,20 @@ public class Config {
 	
 	public static final int ARCHER_RANGE = 2;
 	
-	public static final int SWORDSMEN_STRENGTH = 25;
+	public static final int SWORDSMEN_STRENGTH = 22;
 	public static final int SWORDSMEN_TOUGHNESS = 18;
 	
 	public static final int SPEARMEN_STRENGTH = 15;
-	public static final int SPEARMEN_TOUGHNESS = 25;
+	public static final int SPEARMEN_TOUGHNESS = 30;
 	
-	public static final int ARCHERS_STRENGTH = 18;
+	public static final int ARCHERS_STRENGTH = 20;
 	public static final int ARCHERS_TOUGHNESS = 10;
 	
-	public static final int CAVALRY_STRENGTH = 30;
+	public static final int CAVALRY_STRENGTH = 20;
 	public static final int CAVALRY_TOUGHNESS = 15;
 	
 	public static final int ELITE_STRENGTH = 20;
-	public static final int ELITE_TOUGHNESS = 35;
+	public static final int ELITE_TOUGHNESS = 40;
 
 	public static final int NUM_SWORDSMEN = 2;
 	public static final int NUM_SPEARMEN = 2;
