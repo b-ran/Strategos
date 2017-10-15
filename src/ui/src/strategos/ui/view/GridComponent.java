581d4e6d1be491989259b349a50ed18688edd96a
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2017-10-15 21:08:23 +1300

    Javadoc pass through for components & Add support for smaller screens

--- src/ui/src/strategos/ui/view/GridComponent.java
+++ src/ui/src/strategos/ui/view/GridComponent.java
@@ -11,18 +11,23 @@ import java.util.List;
 
 import static strategos.ui.config.Config.*;
 
+
 /**
- * The type Grid component.
+ * Used to create grid that the board and units are drawn on
+ * @author Brandon Scott-Hill
  */
+
 public class GridComponent extends JComponent {
 
     private final View view;
     private MapLocation[][] terrain;
     private List<Unit> entities;
     private Draw draw;
+
     private MapLocation selectedMapLocation;
-    private List<Unit> unitsInAttackRange = new ArrayList<>();
-    private List<MapLocation> tilesInRange = new ArrayList<>();
+
+    private List<Unit> unitsInAttackRange = new ArrayList<>(); //Attackable units in range of selected map location
+    private List<MapLocation> tilesInRange = new ArrayList<>(); //Tiles in range of selected map location
 
     /**
      * Instantiates a new Grid component for drawing on.
@@ -35,8 +40,9 @@ public class GridComponent extends JComponent {
     }
 
     /**
-     * Gets grid panel.
+     * Gets a layered grid panel.
      *
+     * @author Brandon Scott-Hill
      * @return the grid
      */
     JLayeredPane getGrid() {
@@ -48,18 +54,45 @@ public class GridComponent extends JComponent {
 
     @Override
     protected void paintComponent(Graphics g) {
+        updateHexSize();
         paintBackground(g);
         paintTerrain(g, view.getSeenTerrain());
         paintSelection(g, selectedMapLocation);
         paintUnits(g, entities);
-        paintBlackTerrain(g, terrain);
+        paintFogTerrain(g, terrain);
 
     }
 
+    /**
+     * Updates the hex size to fit in grid size.
+     *
+     * @author Brandon Scott-Hill
+     */
+    private void updateHexSize() {
+        int screenWidth = getWidth();
+        int size = view.model.getWorld().getMap().getDiameter();
+        size += Math.round(size/2.0);
+        HEX_SIZE = screenWidth / size;
+    }
+
+    /**
+     * Draws the background image
+     *
+     * @author Brandon Scott-Hill
+     * @param g the Graphics
+     */
     private void paintBackground(Graphics g) {
         g.drawImage(Draw.backgroundImage, 0, 0, null);
     }
 
+
+    /**
+     * Draws all units in a collection
+     *
+     * @author Brandon Scott-Hill
+     * @param g the Graphics
+     * @param entities the Units to draw
+     */
     private void paintUnits(Graphics g, List<Unit> entities) {
         for (Unit unit : entities) {
             if (!view.getSeenTerrain().contains(unit.getPosition())) {
@@ -72,6 +105,13 @@ public class GridComponent extends JComponent {
         }
     }
 
+    /**
+     * Draws all visible tiles
+     *
+     * @author Brandon Scott-Hill
+     * @param g the Graphics
+     * @param visibleTiles the visibleTiles to draw
+     */
     private void paintTerrain(Graphics g, List<MapLocation> visibleTiles) {
         for (MapLocation tile : visibleTiles) {
             if (tile.getTerrain() == null) continue;
@@ -82,7 +122,14 @@ public class GridComponent extends JComponent {
         }
     }
 
-    private void paintBlackTerrain(Graphics g, MapLocation[][] terrain) {
+    /**
+     * Draws all terrain as fog if they have not being seen
+     *
+     * @author Brandon Scott-Hill
+     * @param g the Graphics
+     * @param terrain the visibleTiles to draw
+     */
+    private void paintFogTerrain(Graphics g, MapLocation[][] terrain) {
         for (int y = 0; y < terrain.length; y++) {
             for (int x = 0; x < terrain[0].length; x++) {
                 if (view.getSeenTerrain().contains(terrain[y][x])) continue;
@@ -94,6 +141,13 @@ public class GridComponent extends JComponent {
         }
     }
 
+    /**
+     * Draws all the selection of the user
+     *
+     * @author Brandon Scott-Hill
+     * @param g the Graphics
+     * @param selectedMapLocation the selected location the board
+     */
     private void paintSelection(Graphics g, MapLocation selectedMapLocation) {
         if (selectedMapLocation == null) return;
         Point p = new Point();
@@ -120,19 +174,27 @@ public class GridComponent extends JComponent {
     /**
      * Sets entities.
      *
+     * @author Brandon Scott-Hill
      * @param entities the entities
      */
     public void setEntities(List<Unit> entities) {
         this.entities = entities;
     }
 
-    public List<Unit> getEntities() {
+    /**
+     * Gets entities.
+     *
+     * @author Brandon Scott-Hill
+     * @return the entities
+     */
+    List<Unit> getEntities() {
         return entities;
     }
 
     /**
      * Sets terrain.
      *
+     * @author Brandon Scott-Hill
      * @param terrain the terrain
      */
     public void setTerrain(MapLocation[][] terrain) {
@@ -141,24 +203,42 @@ public class GridComponent extends JComponent {
     }
 
 
+    /**
+     * Sets selection.
+     *
+     * @author Brandon Scott-Hill
+     * @param selectedMapLocation the selected map location
+     * @param unitsInAttackRange  the units in attack range
+     * @param tilesInRange        the tiles in range
+     */
     public void setSelection(MapLocation selectedMapLocation, List<Unit> unitsInAttackRange, List<MapLocation> tilesInRange) {
         this.selectedMapLocation = selectedMapLocation;
         this.unitsInAttackRange = unitsInAttackRange;
         this.tilesInRange = tilesInRange;
     }
 
+    /**
+     * Sets selection.
+     *
+     * @author Brandon Scott-Hill
+     * @param selectedMapLocation the selected map location
+     */
     public void setSelection(MapLocation selectedMapLocation) {
         this.selectedMapLocation = selectedMapLocation;
         unitsInAttackRange = new ArrayList<>();
         tilesInRange = new ArrayList<>();
     }
 
-    public void revealMap() {
-        for (int y = 0; y < terrain.length; y++) {
-            for (int x = 0; x < terrain[0].length; x++) {
-                view.getSeenTerrain().add(terrain[y][x]);
-            }
-        }
+    /**
+     * Reveal map.
+     * @author Brandon Scott-Hill
+     */
+    void revealMap() {
+         for (MapLocation[] aTerrain : terrain) {
+             for (int x = 0; x < terrain[0].length; x++) {
+                 view.getSeenTerrain().add(aTerrain[x]);
+             }
+         }
 
     }
 }
