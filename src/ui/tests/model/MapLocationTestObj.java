f8dec4175e72b9e2e99b3708a4481dbee892d2d5
Brandon Scott-Hill <scotthbran@ecs.vuw.ac.nz>
2017-10-15 21:06:25 +1300

    Add equals method to map location test object

--- src/ui/tests/model/MapLocationTestObj.java
+++ src/ui/tests/model/MapLocationTestObj.java
@@ -56,5 +56,25 @@ public class MapLocationTestObj implements MapLocation{
     public Map<Direction, MapLocation> getNeighbours() {
         return null;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MapLocationTestObj hex = (MapLocationTestObj) o;
+
+        if (x != hex.getX()) return false;
+        if (y != hex.getY()) return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        result = 31 * result + (terrain != null ? terrain.hashCode() : 0);
+        return result;
+    }
     
 }
